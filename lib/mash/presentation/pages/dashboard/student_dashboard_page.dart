75a30000e025ff0fcdf5c857fc43675d19f3e88e
ayyubi vh <ayyubivh@gmail.com>
2024-04-13 10:23:09 +0530

    fixed some changes

--- lib/mash/presentation/pages/dashboard/student_dashboard_page.dart
+++ lib/mash/presentation/pages/dashboard/student_dashboard_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:mash/mash/presentation/utils/app_colors.dart';
+import 'package:mash/mash/presentation/utils/app_constants.dart';
+import 'package:mash/mash/presentation/utils/size_utility.dart';
 import 'package:mash/mash/presentation/widgets/custom_app_bar.dart';
 
 void main() {
@@ -13,11 +18,94 @@ class StudentDashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: customAppbar(context),
-     body: ListView(children: [
-       Image.asset('assets/images/student_dummy.png')
-     
-     ],),
-          );
+      appBar: customAppbar(context),
+      body: ListView(
+        children: [
+          Image.asset('assets/images/student_dummy.png'),
+          spacer26,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const Icon(Icons.arrow_back_ios),
+              _selectStudentProfileWidget(),
+              _selectStudentProfileWidget(),
+              const Icon(Icons.arrow_forward_ios),
+            ],
+          ),
+          spacer20,
+          const Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Rahul Balakrishnan",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          spacer30,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _nameWidget('Class Teacher', 'Rajani Rajan'),
+                _nameWidget('Roll No', '0394'),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 1,
+                width: SizeUtility(context).width / 5,
+                decoration: BoxDecoration(
+                  gradient: AppColors.lightBlueGradientColors,
+                ),
+              ),
+              Container(
+                height: 1,
+                width: SizeUtility(context).width / 5,
+                decoration: BoxDecoration(
+                  gradient: AppColors.lightBlueGradientColors,
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _nameWidget(String title, String name) {
+    return Column(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+        Text(
+          name,
+          style: const TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _selectStudentProfileWidget() {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration:
+          const BoxDecoration(shape: BoxShape.circle, color: Colors.amber),
+    );
   }
 }
--- lib/mash/presentation/utils/app_constants.dart
+++ lib/mash/presentation/utils/app_constants.dart
@@ -1,29 +1,70 @@
 import 'package:flutter/cupertino.dart';
 
-class AppConstants{
-static const appName ="MASH";
+class AppConstants {
+  static const appName = "MASH";
 }
+
 //
- const spacer10 = SizedBox(height: 10,);
- const spacer20 = SizedBox(height: 20,);
- const spacer30 = SizedBox(height: 30,);
- const spacer40 = SizedBox(height: 40,);
- const spacer50 = SizedBox(height: 50,);
- const spacer60 = SizedBox(height: 60,);
- const spacer70 = SizedBox(height: 70,);
- const spacer80 = SizedBox(height: 80,);
- const spacer90 = SizedBox(height: 90,);
- const spacer100 = SizedBox(height: 100,);
- const spacer110 = SizedBox(height: 110,);
- const spacer120 = SizedBox(height: 120,);
+const spacer10 = SizedBox(
+  height: 10,
+);
+const spacer20 = SizedBox(
+  height: 20,
+);
+const spacer26 = SizedBox(
+  height: 26,
+);
 
- //width
- const spacerWidth10 = SizedBox(height: 10,);
- const spacerWidth20 = SizedBox(height: 20,);
- const spacerWidth30 = SizedBox(height: 30,);
- const spacerWidth40 = SizedBox(height: 40,);
- const spacerWidth50 = SizedBox(height: 50,);
- const spacerWidth60 = SizedBox(height: 60,);
- const spacerWidth70 = SizedBox(height: 70,);
+const spacer30 = SizedBox(
+  height: 30,
+);
+const spacer40 = SizedBox(
+  height: 40,
+);
+const spacer50 = SizedBox(
+  height: 50,
+);
+const spacer60 = SizedBox(
+  height: 60,
+);
+const spacer70 = SizedBox(
+  height: 70,
+);
+const spacer80 = SizedBox(
+  height: 80,
+);
+const spacer90 = SizedBox(
+  height: 90,
+);
+const spacer100 = SizedBox(
+  height: 100,
+);
+const spacer110 = SizedBox(
+  height: 110,
+);
+const spacer120 = SizedBox(
+  height: 120,
+);
 
-
+//width
+const spacerWidth10 = SizedBox(
+  height: 10,
+);
+const spacerWidth20 = SizedBox(
+  height: 20,
+);
+const spacerWidth30 = SizedBox(
+  height: 30,
+);
+const spacerWidth40 = SizedBox(
+  height: 40,
+);
+const spacerWidth50 = SizedBox(
+  height: 50,
+);
+const spacerWidth60 = SizedBox(
+  height: 60,
+);
+const spacerWidth70 = SizedBox(
+  height: 70,
+);
