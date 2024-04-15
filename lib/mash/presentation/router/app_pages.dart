a4bda07caf5299644c07e216777b3da707bb63e0
codernta <ta007nithin@gmail.com>
2024-04-15 15:18:54 +0530

    otp commit

--- lib/mash/presentation/router/app_pages.dart
+++ lib/mash/presentation/router/app_pages.dart
@@ -2,4 +2,5 @@ class AppPages{
   static const splash ="/";
   static const login = "/login";
   static const forgotPassword = "/forgotPassword";
+  static const otpScreen = "/otpScreen";
 }
--- lib/mash/presentation/router/router_config.dart
+++ lib/mash/presentation/router/router_config.dart
@@ -1,6 +1,8 @@
 import 'package:go_router/go_router.dart';
-import 'package:mash/mash/presentation/pages/forgot_password_screen.dart';
-import 'package:mash/mash/presentation/pages/login_screen.dart';
+import 'package:mash/mash/presentation/pages/auth/forgot_password_screen.dart';
+import 'package:mash/mash/presentation/pages/auth/login_screen.dart';
+import 'package:mash/mash/presentation/pages/auth/otp_screen.dart';
+
 import 'package:mash/mash/presentation/router/app_pages.dart';
 
 import '../pages/splash_screen.dart';
@@ -23,6 +25,10 @@ class AppRouteManager {
           path: AppPages.forgotPassword,
           name: AppPages.forgotPassword,
           builder: (context, state) => const ForgotPasswordScreen(),
+        ),GoRoute(
+          path: AppPages.otpScreen,
+          name: AppPages.otpScreen,
+          builder: (context, state) => const OtpScreen(),
         ),
       ]);
 }
