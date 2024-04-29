a36f65d2a4352f65a60a931b37150d427b6f47ed
ayyubi vh <ld-programmer04@manappuram.com>
2024-04-29 11:26:29 +0530

    removed dartz package

--- lib/mash/data/remote/data_sources/dashboard_data_source.dart
+++ lib/mash/data/remote/data_sources/dashboard_data_source.dart
@@ -1,25 +1,23 @@
-import 'package:dartz/dartz.dart';
 import 'package:mash/mash/data/remote/models/dashboard/word_thought_day_model.dart';
-
+import 'package:mash/mash/data/remote/models/request/word_thought_request.dart';
 import '../../../../core/api_provider.dart';
-import '../../../../core/failure.dart';
-import '../../../domain/entities/dashboard/word_thought_entity.dart';
 import '../routes/app_remote_routes.dart';
 
 abstract class DashBoardDataSource {
-   Future<Either<Failure, WordThoughtsModel>> fetchWordandThoghtOfTheDay();
+  Future<WordThoughtsModel> fetchWordandThoghtOfTheDay(
+      WordThoughtRequest wordThoughtRequest);
 }
 
 class DashBoardDataSourceImpl extends DashBoardDataSource {
-      final ApiProvider apiProvider;
+  final ApiProvider apiProvider;
 
   DashBoardDataSourceImpl({required this.apiProvider});
 
   @override
-  Future<Either<Failure, WordThoughtsModel>> fetchWordandThoghtOfTheDay() async {
-    
-
-    final data = await apiProvider.post(formBody: , AppRemoteRoutes.wordAndThougthofTheDay);
-  return Right(WordThoughtsModel.fromJson(data));
+  Future<WordThoughtsModel> fetchWordandThoghtOfTheDay(
+      WordThoughtRequest wordThoughtRequest) async {
+    final data = await apiProvider.post(
+        AppRemoteRoutes.wordAndThougthofTheDay, wordThoughtRequest.toJson());
+    return WordThoughtsModel.fromJson(data);
   }
 }
--- lib/mash/domain/repositories/dash_board_repository_impl.dart
+++ lib/mash/domain/repositories/dash_board_repository_impl.dart
@@ -1,11 +1,9 @@
-import 'package:dartz/dartz.dart';
-import 'package:mash/core/failure.dart';
 import 'package:mash/mash/data/repositories/dash_board_repository.dart';
 import 'package:mash/mash/domain/entities/dashboard/word_thought_entity.dart';
 
 class DashBoardRepoImpl extends DashBoardRepository {
   @override
-  Either<Failure, WordThoughtsEntity> fetchWordandThoghtOfTheDay() {
+  Future<WordThoughtsEntity> fetchWordandThoghtOfTheDay() {
     // TODO: implement fetchWordandThoghtOfTheDay
     throw UnimplementedError();
   }
