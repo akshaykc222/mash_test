515c9dddaf6e0a22a541588a1c0e5d8a273caa1c
ayyubi vh <ld-programmer04@manappuram.com>
2024-05-28 17:29:59 +0530

    fixed splash screen

--- lib/mash/presentation/pages/splash_screen.dart
+++ lib/mash/presentation/pages/splash_screen.dart
@@ -44,12 +44,14 @@
 //     );
 //   }
 // }
-
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:mash/mash/presentation/pages/auth/login_screen.dart';
+import 'package:mash/core/pretty_printer.dart';
 import 'package:mash/mash/presentation/router/app_pages.dart';
+import 'package:mash/mash/presentation/utils/app_colors.dart';
+import '../../../core/hive_service.dart';
+import '../../data/remote/routes/local_storage_name.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -65,6 +67,21 @@ class SplashScreenState extends State<SplashScreen>
 
   double _opacity = 0;
   bool _value = true;
+  double _opacity2 = 1;
+
+  Future<String> _init() async {
+    try {
+      final token =
+          await HiveService().getBox<String>(boxName: LocalStorageNames.token);
+      if (token.isNotEmpty) {
+        return AppPages.home;
+      } else {
+        return AppPages.login;
+      }
+    } catch (e) {
+      return AppPages.login;
+    }
+  }
 
   @override
   void initState() {
@@ -73,22 +90,19 @@ class SplashScreenState extends State<SplashScreen>
     scaleController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 600),
-    )..addStatusListener(
-        (status) {
-          if (status == AnimationStatus.completed) {
-            context.pushNamed(AppPages.login);
-            Timer(
-              const Duration(milliseconds: 300),
-              () {
-                scaleController.reset();
-              },
-            );
-          }
-        },
-      );
+    )..addStatusListener((status) async {
+        prettyPrint(status.toString());
+        if (status == AnimationStatus.completed) {
+          final routeName = await _init();
+          context.pushNamed(routeName);
+          Timer(const Duration(milliseconds: 300), () {
+            scaleController.reset();
+          });
+        }
+      });
 
     scaleAnimation =
-        Tween<double>(begin: 0.0, end: 12).animate(scaleController);
+        Tween<double>(begin: 0.0, end: 10).animate(scaleController);
 
     Timer(const Duration(milliseconds: 600), () {
       setState(() {
@@ -96,6 +110,13 @@ class SplashScreenState extends State<SplashScreen>
         _value = false;
       });
     });
+
+    Timer(const Duration(milliseconds: 100), () {
+      setState(() {
+        _opacity2 = 0;
+      });
+    });
+
     Timer(const Duration(milliseconds: 2000), () {
       setState(() {
         scaleController.forward();
@@ -112,59 +133,47 @@ class SplashScreenState extends State<SplashScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: AnimatedOpacity(
+        duration: const Duration(seconds: 3),
+        opacity: _opacity2,
+        child: Container(
+          color: AppColors.primaryColor,
+          child: Stack(
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 80),
-                child: Text(
-                  'Here will be your app\'s logo',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Center(
-            child: AnimatedOpacity(
-              curve: Curves.fastLinearToSlowEaseIn,
-              duration: const Duration(seconds: 6),
-              opacity: _opacity,
-              child: AnimatedContainer(
-                curve: Curves.fastLinearToSlowEaseIn,
-                duration: const Duration(seconds: 2),
-                height: _value ? 50 : 200,
-                width: _value ? 50 : 200,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.deepPurpleAccent.withOpacity(.2),
-                      blurRadius: 100,
-                      spreadRadius: 10,
-                    ),
-                  ],
-                  color: Colors.deepPurpleAccent,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Center(
-                  child: Container(
-                    width: 100,
-                    height: 100,
+              Center(
+                child: AnimatedOpacity(
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  duration: const Duration(seconds: 6),
+                  opacity: _opacity,
+                  child: AnimatedContainer(
+                    curve: Curves.fastLinearToSlowEaseIn,
+                    duration: const Duration(seconds: 2),
+                    height: _value ? 50 : 100,
+                    width: _value ? 50 : 100,
                     decoration: BoxDecoration(
-                        color: Colors.deepPurpleAccent, shape: BoxShape.circle),
-                    child: AnimatedBuilder(
-                      animation: scaleAnimation,
-                      builder: (c, child) => Transform.scale(
-                        scale: scaleAnimation.value,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.deepPurpleAccent,
+                      image: const DecorationImage(
+                        image: AssetImage('assets/icons/mash_icon.png'),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: AppColors.white,
+                          blurRadius: 100,
+                          spreadRadius: 10,
+                        ),
+                      ],
+                      color: AppColors.primaryColor,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Center(
+                      child: AnimatedBuilder(
+                        animation: scaleAnimation,
+                        builder: (c, child) => Transform.scale(
+                          scale: scaleAnimation.value,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: AppColors.white,
+                            ),
                           ),
                         ),
                       ),
@@ -172,9 +181,9 @@ class SplashScreenState extends State<SplashScreen>
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
